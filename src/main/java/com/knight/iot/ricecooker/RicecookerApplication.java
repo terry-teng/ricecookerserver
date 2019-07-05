d2b9b0f3e16288d1b4ce7ebe5f83009b38f2db30
Terry Teng <tteng@xperigo.com>
2019-07-05 12:57:24 -0400

    git test

---
+++ src/main/java/com/knight/iot/ricecooker/RicecookerApplication.java
@@ -0,0 +1,13 @@
+package com.knight.iot.ricecooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicecookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RicecookerApplication.class, args);
+	}
+
+}
